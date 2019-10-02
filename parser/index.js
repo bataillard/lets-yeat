a7b8133eb3671e7de020813ad4c7881967513690
Luca Bataillard <lucabad@bluewin.ch>
2019-10-02 16:33:44 -0700

    fully parse budgetbytes

--- parser/index.js
+++ parser/index.js
@@ -2,28 +2,44 @@
 
 const rp = require('request-promise');
 const $ = require('cheerio');
-const url = "https://www.budgetbytes.com/ground-turkey-stir-fry/";
 
-main(url);
+// =================================
+function main() {
+    const url = process.argv[2];
+    
+    parseBudgetBytesUrl(url).then(recipe => console.log(recipe))
+}
 
-/* 
+main();
+// =================================
+
+/**
+ * Set of possible tags recognized by the system, can be expanded as needed 
+ */
+const possible_tags = new Set([
+    "beef", "turkey", "chicken", "fish", "pork", "vegan", "vegertarian", "easy", "quick", 
+    "aisan", "chinese", "japanese", "thai", "vietnamese", "pasta", "pizza"
+]);
+
+/**
  * Represents basic recipe following the scheme defined as:
  *
  * : Recipe {
- *   name: String,
- *   pictureUrl: String            // URL pointing to image
- *   time: Int,                 // Number of minutes
- *   difficulty: Float,         // In range 0-5
+ *   name: string,
+ *   pictureUrl: string         // URL pointing to image
+ *   time: int,                 // Number of minutes
+ *   difficulty: float,         // In range 0-5
  *   ingredients: [Ingredient]  // List of ingredient objects 
- *   instructions: [String]     // Ordered list of steps
- *   tags: [String]             // List of tags for each recipe
+ *   instructions: [string]     // Ordered list of steps
+ *   tags: [string]             // List of tags for each recipe
  * }
  * 
  * inspired by https://schema.org/Recipe 
  * This will then be transmitted to the database to be stored
  */
 class Recipe {
-    constructor(name, pictureUrl, time, difficulty, ingredients, instructions, tags) {
+    constructor(url, name, pictureUrl, time, difficulty, ingredients, instructions, tags) {
+        this.url = url;
         this.name = name;
         this.pictureUrl = pictureUrl;
         this.time = time;
@@ -34,20 +50,69 @@ class Recipe {
     }
 }
 
-function main(url) {
-    rp(url)
-    .then((html) => {
+/**
+ * Represents each ingredient
+ * :Ingredient {
+ *   name: string,
+ *   quantity: number,
+ *   unit: string
+ * }
+ */
+class Ingredient {
+
+    /**
+     * Converts quantity into a floating point number
+     * @param {string, float} quantity 
+     * @return {float, null} float or null if uncountable quantity (salt)
+     */
+    static convertQuantity(quantity) {
+        const fractionRegExp = /(\d+)\s*\/\s*(\d+)/;
+        const numberRegExp = /\d+[.]?/;
+        
+        if (typeof quantity === "string") {
+            if (fractionRegExp.test(quantity)) {
+                const match = quantity.match(fractionRegExp);
+                return parseFloat(match[1]) / parseFloat(match[2]);
+            }
+
+            if (quantity.length == 0) {
+                return null;
+            }
+
+            return parseFloat(quantity.match(numberRegExp)[0]);
+
+        } else {
+            return quantity;
+        }
+    }
+
+    constructor(name, quantity, unit) {
+        this.name = name.replace(/[*]/g, "");
+        this.quantity = this.constructor.convertQuantity(quantity);
+        this.unit = unit;
+    }
+}
+
+/**
+ * Returns a promise to a Recipe from given url
+ * @param url 
+ */
+function parseBudgetBytesUrl(url) {
+    return rp(url).then(html => {
         const pictureUrl = parseImgSrc(html);
+        const tags = parseTags(html);
+
         const recipe = $(".wprm-recipe-container > .wprm-recipe", html);
 
         const name = $(".wprm-recipe-name", recipe).text();
         const time = parseTime(recipe);
+        const difficulty = 3;
+        const ingredients = parseIngredients(recipe);
+        const instructions = parseInstructions(recipe);
         
-        console.log(new Recipe(name, pictureUrl, time, null, [], [], []));
+        return new Recipe(url, name, pictureUrl, time, difficulty, 
+            ingredients, instructions, tags);
     })
-    .catch((err) => {
-        console.log(err);
-    });
 }
 
 function parseImgSrc(html) {
@@ -56,6 +121,21 @@ function parseImgSrc(html) {
 
     return parser("img").attr("src");
 
+}
+
+function parseTags(html) {
+    function extractBreadcrumbs(html) {
+        return $("#breadcrumbs", html).text();
+    }
+
+    const words = extractBreadcrumbs(html).split(" ")   // split into words
+        .map(s => s.replace(/\W/g, "").toLowerCase())   // remove special chars
+        .filter(s => s.length > 0)                      // remove empty words
+    
+    // Intersection of words and potential tags
+    const tags = [...new Set(words)].filter(w => possible_tags.has(w));
+    
+    return tags;
 }
 
 function parseTime(recipe) {
@@ -67,5 +147,30 @@ function parseTime(recipe) {
     return 60*convert(hoursStr) + convert(minutesStr);
 }
 
+function parseIngredients(recipe) {
+    const ingredients = [];
+
+    $("li.wprm-recipe-ingredient", recipe).each((_, e) => {
+        const amount = $(e).find(".wprm-recipe-ingredient-amount").text();
+        const name = $(e).find(".wprm-recipe-ingredient-name").text();
+
+        const unitSel = $(e).find(".wprm-recipe-ingredient-unit");
+        const unit = unitSel.length == 0 ? null : unitSel.text();
+
+        ingredients.push(new Ingredient(name, amount, unit));
+    });
+
+    return ingredients;
+}
+
+function parseInstructions(recipe) {
+    const instructions = []
+
+    $("li.wprm-recipe-instruction", recipe).each((i, elem) => {
+        instructions.push($(elem).text());
+    });
+
+    return instructions;
+}
 
 
