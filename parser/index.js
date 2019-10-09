ff9b9144352007474e8b9296deac02509aeed112
Luca Bataillard <lucabad@bluewin.ch>
2019-10-09 11:49:39 -0700

    implement batch parsing for budgetbytes

--- parser/index.js
+++ parser/index.js
@@ -1,176 +1,56 @@
 'use strict';
 
-const rp = require('request-promise');
-const $ = require('cheerio');
+const budget = require('./budgetbytes.js');
+const url_parser = require('url');
 
-// =================================
+/**
+ * Usage: 
+ *  single recipe: "node index.js budget recipe `url`"
+ *  by date:       "node index.js budget date `max` `fromYear` `fromMonth` `toYear` `toMonth`"
+ */
 function main() {
-    const url = process.argv[2];
+    const website = process.argv[2];
+    const command = process.argv[3];
+
+    if (website !== "budget") {
+        console.error("Website not supported");
+        return;
+    }
+
+    if (command === "recipe") {
+        const urlString = process.argv[4];
+        let url; 
+
+        try {
+            url = url_parser.parse(urlString);
+        } catch (err) {
+            console.error(err);
+            console.error("Could not parse URL: " + urlString);
+            return;
+        }
     
-    parseBudgetBytesUrl(url).then(recipe => console.log(recipe))
+        if (url.hostname === "www.budgetbytes.com") {
+            budget.parseUrl(url.href).then(recipe => console.log(recipe))
+        } else {
+            console.error("Unsupported URL");
+            return;
+        }
+    } else if (command === "date") {
+        const max = parseInt(process.argv[4]);
+        const fy = parseInt(process.argv[5]);
+        const fm = parseInt(process.argv[6]);
+        const ty = parseInt(process.argv[7]);
+        const tm = parseInt(process.argv[8]);
+
+        budget.parseByDate(max, fy, fm, ty, tm).then(console.log);
+    } else {
+        console.error("Command not supported");
+        return;
+    }
 }
 
 main();
-// =================================
 
-/**
- * Set of possible tags recognized by the system, can be expanded as needed 
- */
-const possible_tags = new Set([
-    "beef", "turkey", "chicken", "fish", "pork", "vegan", "vegertarian", "easy", "quick", 
-    "aisan", "chinese", "japanese", "thai", "vietnamese", "pasta", "pizza"
-]);
 
-/**
- * Represents basic recipe following the scheme defined as:
- *
- * : Recipe {
- *   name: string,
- *   pictureUrl: string         // URL pointing to image
- *   time: int,                 // Number of minutes
- *   difficulty: float,         // In range 0-5
- *   ingredients: [Ingredient]  // List of ingredient objects 
- *   instructions: [string]     // Ordered list of steps
- *   tags: [string]             // List of tags for each recipe
- * }
- * 
- * inspired by https://schema.org/Recipe 
- * This will then be transmitted to the database to be stored
- */
-class Recipe {
-    constructor(url, name, pictureUrl, time, difficulty, ingredients, instructions, tags) {
-        this.url = url;
-        this.name = name;
-        this.pictureUrl = pictureUrl;
-        this.time = time;
-        this.difficulty = difficulty;
-        this.ingredients = [...ingredients];
-        this.instruction = [...instructions];
-        this.tags = [...tags];
-    }
-}
-
-/**
- * Represents each ingredient
- * :Ingredient {
- *   name: string,
- *   quantity: number,
- *   unit: string
- * }
- */
-class Ingredient {
-
-    /**
-     * Converts quantity into a floating point number
-     * @param {string, float} quantity 
-     * @return {float, null} float or null if uncountable quantity (salt)
-     */
-    static convertQuantity(quantity) {
-        const fractionRegExp = /(\d+)\s*\/\s*(\d+)/;
-        const numberRegExp = /\d+[.]?/;
-        
-        if (typeof quantity === "string") {
-            if (fractionRegExp.test(quantity)) {
-                const match = quantity.match(fractionRegExp);
-                return parseFloat(match[1]) / parseFloat(match[2]);
-            }
-
-            if (quantity.length == 0) {
-                return null;
-            }
-
-            return parseFloat(quantity.match(numberRegExp)[0]);
-
-        } else {
-            return quantity;
-        }
-    }
-
-    constructor(name, quantity, unit) {
-        this.name = name.replace(/[*]/g, "");
-        this.quantity = this.constructor.convertQuantity(quantity);
-        this.unit = unit;
-    }
-}
-
-/**
- * Returns a promise to a Recipe from given url
- * @param url 
- */
-function parseBudgetBytesUrl(url) {
-    return rp(url).then(html => {
-        const pictureUrl = parseImgSrc(html);
-        const tags = parseTags(html);
-
-        const recipe = $(".wprm-recipe-container > .wprm-recipe", html);
-
-        const name = $(".wprm-recipe-name", recipe).text();
-        const time = parseTime(recipe);
-        const difficulty = 3;
-        const ingredients = parseIngredients(recipe);
-        const instructions = parseInstructions(recipe);
-        
-        return new Recipe(url, name, pictureUrl, time, difficulty, 
-            ingredients, instructions, tags);
-    })
-}
-
-function parseImgSrc(html) {
-    const noscriptTag = $(".post > p > noscript", html).get(0).children[0].data;
-    const parser = $.load(noscriptTag, {xmlMode: true});
-
-    return parser("img").attr("src");
-
-}
-
-function parseTags(html) {
-    function extractBreadcrumbs(html) {
-        return $("#breadcrumbs", html).text();
-    }
-
-    const words = extractBreadcrumbs(html).split(" ")   // split into words
-        .map(s => s.replace(/\W/g, "").toLowerCase())   // remove special chars
-        .filter(s => s.length > 0)                      // remove empty words
-    
-    // Intersection of words and potential tags
-    const tags = [...new Set(words)].filter(w => possible_tags.has(w));
-    
-    return tags;
-}
-
-function parseTime(recipe) {
-    const hoursStr = $(".wprm-recipe-total_time-hours", recipe).text();
-    const minutesStr = $(".wprm-recipe-total_time-minutes", recipe).text();
-    
-    const convert = (value) => value === "" ? 0 : parseInt(value)
-
-    return 60*convert(hoursStr) + convert(minutesStr);
-}
-
-function parseIngredients(recipe) {
-    const ingredients = [];
-
-    $("li.wprm-recipe-ingredient", recipe).each((_, e) => {
-        const amount = $(e).find(".wprm-recipe-ingredient-amount").text();
-        const name = $(e).find(".wprm-recipe-ingredient-name").text();
-
-        const unitSel = $(e).find(".wprm-recipe-ingredient-unit");
-        const unit = unitSel.length == 0 ? null : unitSel.text();
-
-        ingredients.push(new Ingredient(name, amount, unit));
-    });
-
-    return ingredients;
-}
-
-function parseInstructions(recipe) {
-    const instructions = []
-
-    $("li.wprm-recipe-instruction", recipe).each((i, elem) => {
-        instructions.push($(elem).text());
-    });
-
-    return instructions;
-}
 
 
