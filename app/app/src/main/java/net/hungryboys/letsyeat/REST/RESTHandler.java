69a95c64963d0663193628de4ad3e2dd618272a6
meccleston <martinitan@hotmail.com>
2019-10-16 16:14:14 -0700

    added declarations for REST API class and methods

---
+++ app/app/src/main/java/net/hungryboys/letsyeat/REST/RESTHandler.java
@@ -0,0 +1,54 @@
+package net.hungryboys.letsyeat.REST;
+
+import org.json.JSONObject;
+
+//
+// This Class implements a REST API for the application
+//
+public class RESTHandler {
+
+
+    //gets a single recipe
+    public JSONObject getRecipe(){
+        return null;
+    }
+
+    //gets a single random recipe
+    public JSONObject getRecipeSuggestion(){
+        return null;
+    }
+
+    //gets a list of recipes
+    public JSONObject getRecipeList(){
+        return null;
+    }
+
+    //creates a new notification in the server
+    public void putNotification(){
+    }
+
+    //gets user data from server
+    public JSONObject getUSer(){
+        return null;
+    }
+
+    //creates a new user on the server
+    public void putUser(){
+    }
+
+    //updates a user on the server
+    public void postUser(){
+    }
+
+    //gets user login confirmation with server using google login
+    public JSONObject getUserLoginGoogle(){
+        return null;
+    }
+
+    //gets user login confirmation with server using email/pass
+    public JSONObject getUserLogin(){
+        return null;
+    }
+
+
+}
