48d27a88f7d5c93e19c90c7ce06bb4ae995f1a94
Timothy Cheng <timothycheng222@gmail.com>
2019-10-30 23:56:03 -0700

    placeholder functions completed in foodnetwork

--- parser/foodnetwork.js
+++ parser/foodnetwork.js
@@ -1,5 +1,5 @@
 /**
- * This file parses foodnetwork.ca into Recipe objects
+ * This file parses foodnetwork.com into Recipe objects
  * - one of 5 parsers that make up the non-trivial component of
  *   CPEN 321 Let's Yeat, software engineering project.
  */
@@ -9,5 +9,63 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const Recipe = require('./recipe.js').Recipe;
 const Ingredient = require('./recipe.js').Ingredient
- 
 
+export {getRecipes};
+
+ // ================================ Navigating Site ================================= //
+const FOODNETWORK_BASE = "https://www.foodnetwork.ca/everyday-cooking/recipes/"
+
+/**
+ * input: number of reqested recipes
+ * return: array containing 
+ */
+function getRecipes(number_of_recipes){
+
+
+    // place holder
+    return {};
+}
+
+
+
+// ================================ Single Recipe Parsing ================================= //
+
+/**
+ * Parsing a single recipe from url
+ * - fn stands for food network
+ * input: url of single food network recipe
+ * return: one Recipe object
+ */
+
+function parseRecipeFromUrl(fn_url){
+
+}
+
+/**
+ * input: 
+ * return: instructions in array
+ */
+function parseCookingInstructions(){
+
+}
+
+/**
+ * 
+ */
+function parseCookingTime(){
+
+}
+
+/**
+ * 
+ */
+function parseIngredients(){
+
+}
+
+/**
+ * 
+ */
+function parseRecipeImage(){
+
+}
