2f04b61e5bd8858cb1e65f3ed1fde436a97340bd
Luca Bataillard <lucabad@bluewin.ch>
2019-11-17 23:04:26 -0800

    create ui tests for external api usecase

--- app/app/src/androidTest/java/net/hungryboys/letsyeat/ui/LoginTest.java
+++ app/app/src/androidTest/java/net/hungryboys/letsyeat/ui/LoginTest.java
@@ -7,7 +7,6 @@ import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
-import androidx.test.uiautomator.SearchCondition;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.Until;
 
@@ -20,24 +19,40 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.regex.Pattern;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginTest {
 
-    private static final long DEFAULT_TIMEOUT = 1000;
+    private static final long DEFAULT_TIMEOUT = 5000;
+    private static final String LOGIN_PACKAGE = "net.hungryboys.letsyeat.login";
 
-    private UiDevice mUiDevice;
+    private UiDevice device;
 
     @Before
     public void before() {
-        mUiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        // Initialize UiDevice instance
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        device.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = device.getLauncherPackageName();
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), DEFAULT_TIMEOUT);
+
     }
 
 
@@ -63,7 +78,34 @@ public class LoginTest {
 
         onView(withId(R.id.google_login_button)).perform(click());
         intended(toPackage("com.google.android.gms"));
-        mUiDevice.wait(Until.findObject(By.text("Choose an account")), DEFAULT_TIMEOUT);
-        mUiDevice.pressBack();
+        device.wait(Until.findObject(By.pkg("com.google.android.gms")), DEFAULT_TIMEOUT);
+        device.pressBack();
+    }
+
+    @Test
+    public void logsInWithGoogleAndChangesActivity() {
+        launchActivityAsLoggedOut();
+
+        onView(withId(R.id.google_login_button)).perform(click());
+        device.wait(Until.findObject(By.pkg("com.google.android.gms").depth(0)), DEFAULT_TIMEOUT);
+        device.findObject(By.textContains("@")).click();
+        device.wait(Until.findObject(By.pkg(Pattern.compile("net.hungryboys.letsyeat.(registration|browse)"))), DEFAULT_TIMEOUT);
+
+        LoginRepository repo = LoginRepository.getInstance(intentsRule.getActivity().getApplicationContext());
+        assertTrue(repo.isLoggedIn());
+    }
+
+    @Test
+    public void doesNotLoginWithGoogleWhenCanceled() {
+        launchActivityAsLoggedOut();
+
+        onView(withId(R.id.google_login_button)).perform(click());
+        device.wait(Until.findObject(By.pkg("com.google.android.gms").depth(0)), DEFAULT_TIMEOUT);
+        device.pressBack();
+
+        // Check toast appears
+        onView(withText(R.string.login_failed))
+                .inRoot(withDecorView(not(intentsRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
     }
 }
