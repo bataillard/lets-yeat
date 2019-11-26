c46d8b36a64c1ace32e0a9c2e8e3666ec8a43c51
meccleston <martinitan@hotmail.com>
2019-11-25 16:59:54 -0800

    food network mostly done

--- parser/tests/foodnetwork.test.js
+++ parser/tests/foodnetwork.test.js
@@ -12,7 +12,7 @@ jest.dontMock("fs");
 
 var testUrl = "http://www.foodnetwork.ca/recipe/chicken-quesadillas-with-pico-de-gallo/23062/";
 var testImg = "media.foodnetwork.ca/recipetracker/403fe151-6790-4e3c-90fc-36783dc27c4f_chicken-quesadillas_webready.jpg";
-
+var testName = "Chicken Quesadillas with Pico de Gallo"
 
 test("parse correct tags", () => {
 	expect(fn.parseTags(cheerio.load(testHtml))).toStrictEqual(["cheese", "chicken"]);
@@ -32,6 +32,16 @@ test("parse correct cooking instructions", () => {
 	expect(fn.parseCookingInstructions(cheerio.load(testHtml))[7]).toBe("3. Slice the lime in half and squeeze the juice from half the lime into the bowl. Sprinkle with salt, and stir together until combined. Be sure to taste the pico de gallo and adjust the seasonings, adding salt or more diced jalapeno if needed.");
 });
 
-test("parse recipe from URL", () => {
-	
+test("parse recipe from URL", async () => {
+	var recipe_promise = fn.parseRecipeFromUrl(testUrl);
+	var recipe = await recipe_promise.then();
+	expect(recipe.url).toBe(testUrl);
+	expect(recipe.name).toBe(testName);
+	expect(recipe.tags).toStrictEqual(["cheese", "chicken"]);
+});
+
+test("", async () => {
+	var recipe_Url_promise = fn.getRecipeUrls(testUrl);
+	var recipe_Url = await recipe_Url_promise.then();
+	expect(recipe_Url[0]).toBe("https://www.foodnetwork.ca/recipe/stewed-chicken-refried-beans-and-oaxaca-cheese-quesadillas/23063/");
 });
